4b098531b2fea6094f2e7fe70449fc4d3cb15be3
lusentis <lusenti.s@gmail.com>
2013-03-14 14:58:33 +0100

    File upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
-/*jshint browser:true, indent:2, laxcomma:true, jquery:true */
+/*jshint browser:true, indent:2, laxcomma:true, jquery:true, devel:true */
 
 $(function () {
-	
-	
-	
+  var json;
+  
+  
+  var _createTable = function (json) {
+    var $table = $('#table').show();
+  };
+  
+  
+  var _handleFileSelect = function (evt) {
+    var file = evt.target.files[0];
+
+    if (!file || file.length > 500 * 1000 || file.type !== 'application/json' || file.name.indexOf('.json') === -1) {
+      alert('I\'m sorry but the file format you have uploaded is not supported (or your browser is too old).');
+      return;
+    }
+    
+    var reader = new FileReader();
+    reader.addEventListener('load', function (e) {
+      try {
+        json = JSON.parse(e.target.result);
+      } catch (err) {
+        alert('The file you uploaded is not valid (technical details: JSON decode error)');
+        throw err;
+      }
+    });
+    
+    reader.readAsText(file);
+  };
+
+  
+  $('#file')[0].addEventListener('change', _handleFileSelect, false);
+  
 });
 
 
